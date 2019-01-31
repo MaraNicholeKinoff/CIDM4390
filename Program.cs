13c16e4b775ca297414cccc43d82b2940949e60a
Mara Kinoff <maranicholemayfield@gmail.com>
2019-01-30 22:29:53 -0600

    fixing issues with tables

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+
+using Assignment2.Models;
 
 namespace Assignment2
 {
@@ -14,11 +17,31 @@ namespace Assignment2
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+
+            var host = BuildWebHost(args);
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    DataSeeder.Initialize(services);
+                }
+                catch(Exception exp)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(exp, "An error occurred seeding the Database");
+
+                }
+            }
+
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .Build();
     }
 }
--- STEPS.md
+++ STEPS.md
@@ -3,8 +3,10 @@
 1. dotnet new mvc
 2. dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
 3. dotnet tool install --global dotnet-aspnet-codegenerator
-4. Create the Member.cs and Client.cs models.
+4. Create the Member.cs, Client.cs, and SeedData.cs models.
 5. dotnet aspnet-codegenerator controller -name MemberController -m Member -dc AppDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
     - This is scaffolding the Member.
 6. dotnet aspnet-codegenerator controller -name ClientController -m Client -dc AppDbContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries
     - This is scaffolding the Client
+7. dotnet add package Microsoft.EntityFrameworkCore
+8. dotnet add package Microsoft.EntityFrameworkCore.Sqlite
